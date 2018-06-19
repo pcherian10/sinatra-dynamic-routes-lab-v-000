34ebbb51870484925b49d8a07bfc1e3b919fbc0e
Paul <pcherian10@gmail.com>
2018-06-19 23:25:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,7 +23,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5] + "."
+    @new_phrase = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5] + "."
+    @new_phrase
   end
 
 end
